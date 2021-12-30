f1d4a644c9710398546e9e9a1a324bdc6f0395e1
XieJiSS <XieJiSS@users.noreply.github.com>
2021-12-30 21:44:51 +0800

    fix: typo

--- src/plct-archrv-bot.js
+++ src/plct-archrv-bot.js
@@ -331,7 +331,7 @@ onText(/^\/(?:merge|drop)\s+(\S+)$/, async (msg, match) => {
   await replyMessage(chatId, msgId, toSafeMd(`你还没有认领任何 package`));
 });
 
-const MARK_REGEXP = /^\/mark\s+(\S+)\s+(\S+)(\s+[\S\s]+)$/;
+const MARK_REGEXP = /^\/mark\s+(\S+)\s+(\S+)(\s+[\S\s]+)?$/;
 
 onText(MARK_REGEXP, async (msg, match) => {
   const chatId = msg.chat.id;
