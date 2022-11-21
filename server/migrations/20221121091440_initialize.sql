3ed6182f3a219ac0f90e955c5b364df797fdabbb
Avimitin <avimitin@gmail.com>
2022-11-21 19:01:31 +0800

    fix: fix migration syntax
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- server/migrations/20221121091440_initialize.sql
+++ server/migrations/20221121091440_initialize.sql
@@ -1,21 +1,21 @@
 -- Add migration script here
-CREATE TABLE IF NOT EXIST packager(
+CREATE TABLE IF NOT EXISTS packager(
   tg_uid INT NOT NULL UNIQUE PRIMARY KEY,
   alias TEXT NOT NULL
 );
 
-CREATE TALE IF NOT EXIST pkg (
+CREATE TABLE IF NOT EXISTS pkg (
   id INT AUTO INCREMENT PRIMARY KEY,
   name TEXT NOT NULL,
   assignee INT,
   FOREIGN KEY(assignee) REFERENCES packager(tg_uid)
 );
 
-CREATE TABLE IF NOT EXIST mark(
+CREATE TABLE IF NOT EXISTS mark(
   name TEXT NOT NULL,
   mark_by INT,
   comment TEXT,
-  for_pkg INT
+  for_pkg INT,
 
   FOREIGN KEY(mark_by) REFERENCES packager(tg_uid),
   FOREIGN KEY(for_pkg) REFERENCES pkg(id)
