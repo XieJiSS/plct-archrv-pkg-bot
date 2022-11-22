336eca3372f20122120fb86f8f8cff1c9859ac67
Avimitin <avimitin@gmail.com>
2022-11-22 12:14:12 +0800

    docs: add document for each fn and struct
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- server/src/routes.rs
+++ server/src/routes.rs
@@ -1,14 +1,18 @@
 use super::sql;
-use std::collections::HashMap;
 
 use actix_web::{get, HttpResponse};
 
+/// Runtime necessary data.
 pub struct State {
+    /// connection pool to the sqlite database
     pub db_conn: sqlx::SqlitePool,
 }
 
+/// Alias of the application state data
 type Data = actix_web::web::Data<State>;
 
+/// Default JSON response when some internal error occur. The msg field should contains friendly
+/// hint for debugging. And detail field contains the original error.
 #[derive(serde::Serialize)]
 struct ErrorJsonResp<'m> {
     msg: &'m str,
@@ -17,14 +21,25 @@ struct ErrorJsonResp<'m> {
 
 #[get("/add")]
 pub(super) async fn add() -> HttpResponse {
-    HttpResponse::Ok().json(HashMap::from([("msg", "/add")]))
+    todo!()
 }
 
+/// Present the JSON response for route `/pkg`.
+///
+/// The workList contains the package assignment status. And markList contains the marks for each
+/// package.
+#[derive(serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+struct PkgJsonResponse {
+    work_list: Vec<sql::WorkListUnit>,
+}
+
+/// Route `/pkg`
 #[get("/pkg")]
 pub(super) async fn pkg(data: Data) -> HttpResponse {
     let data = sql::get_working_list(&data.db_conn).await;
     match data {
-        Ok(data) => HttpResponse::Ok().json(data),
+        Ok(data) => HttpResponse::Ok().json(PkgJsonResponse { work_list: data }),
         Err(err) => HttpResponse::InternalServerError().json(ErrorJsonResp {
             msg: "fail to get working list",
             detail: err.to_string(),
@@ -34,5 +49,5 @@ pub(super) async fn pkg(data: Data) -> HttpResponse {
 
 #[get("/delete")]
 pub(super) async fn delete() -> HttpResponse {
-    HttpResponse::Ok().json(HashMap::from([("msg", "/delete")]))
+    todo!()
 }
--- server/src/sql.rs
+++ server/src/sql.rs
@@ -1,17 +1,20 @@
 use sqlx::{sqlite::SqliteRow, Row, SqlitePool};
 
+/// Information of a packager
 #[derive(sqlx::FromRow)]
 pub struct Packager {
     pub tg_uid: i64,
     pub alias: String,
 }
 
+/// A single unit of the workList
 #[derive(serde::Serialize)]
 pub struct WorkListUnit {
     alias: String,
     assign: Vec<String>,
 }
 
+/// Get list of packager and their assigned packages
 pub async fn get_working_list(db_conn: &SqlitePool) -> anyhow::Result<Vec<WorkListUnit>> {
     let packager: Vec<Packager> = sqlx::query_as("SELECT * FROM packager")
         .fetch_all(db_conn)
