f2a8920346985315c8d37cb3eaa329afab4c249c
Avimitin <avimitin@gmail.com>
2022-11-22 11:19:51 +0800

    chore: format for readability
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- server/migrations/20221121091440_initialize.sql
+++ server/migrations/20221121091440_initialize.sql
@@ -1,23 +1,23 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS packager(
   tg_uid INT NOT NULL UNIQUE PRIMARY KEY,
-  alias TEXT NOT NULL
+  alias  TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS pkg (
-  id INT AUTO INCREMENT PRIMARY KEY,
-  name TEXT NOT NULL,
-  assignee INT,
+  id               INT AUTO INCREMENT PRIMARY KEY,
+  name             TEXT NOT NULL,
+  assignee         INT,
   last_assigned_at INT,
   FOREIGN KEY(assignee) REFERENCES packager(tg_uid)
 );
 
 CREATE TABLE IF NOT EXISTS mark(
-  name TEXT NOT NULL,
-  mark_by INT,
+  name      TEXT NOT NULL,
+  mark_by   INT,
   marked_at INT NOT NULL,
-  comment TEXT,
-  for_pkg INT,
+  comment   TEXT,
+  for_pkg   INT,
 
   FOREIGN KEY(mark_by) REFERENCES packager(tg_uid),
   FOREIGN KEY(for_pkg) REFERENCES pkg(id)
